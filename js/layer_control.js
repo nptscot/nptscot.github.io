3632dfa41e6ddeaa7b4e4abb19e32959a81c9259
Malcolm Morgan <mem48@users.noreply.github.com>
2023-01-19 12:02:43 +0000

    Fix bug and add gradient

--- js/layer_control.js
+++ js/layer_control.js
@@ -31,15 +31,11 @@ function switch_rnet() {
           'source-layer': 'rnet',
           'paint': {
             'line-color': ["step", ["get", layerId],
-              "rgba(0,0,0,0)", 1,
-              "#9C9C9C", 0,
-              "#FFFF73", 50,
-              "#AFFF00", 100,
-              "#00FFFF", 250,
-              "#30B0FF", 500,
-              "#2E5FFF", 1000,
-              "#0000FF", 2000,
-              "#FF00C5"],
+              "#882255", 25,
+              "#CC6677", 50,
+              "#44AA99", 75,
+              "#117733", 101,
+              "#000000"],
             "line-width": [
               "interpolate",
               ["linear"],
@@ -53,10 +49,55 @@ function switch_rnet() {
             ],
           }
         });
-    
+        
+        document.getElementById("rnetlegend").innerHTML = `<button onclick="show_rnet_legend(false)" style="float:right" aria-label="Hide legend"><i class="fas fa-times"></i></button>
+        <h4>Quietness</h4>
+        <div><span style="background-color: #882255;" class="legenddot"></span>0-25</div>
+    	  <div><span style="background-color: #CC6677;" class="legenddot"></span>25-50</div>
+    	  <div><span style="background-color: #44AA99;" class="legenddot"></span>50-75</div>
+    	  <div><span style="background-color: #117733;" class="legenddot"></span>75-100</div>`;
+    	  
         break;
-      case 'Hilliness':
-        switch_routes();
+      case 'Gradient':
+        map.addLayer({
+          'id': 'rnet',
+          'type': 'line',
+          'source': 'rnet',
+          'source-layer': 'rnet',
+          'paint': {
+            'line-color': ["step", ["get", layerId],
+              "#feebe2", 0,
+              "#fcc5c0", 2,
+              "#fa9fb5", 4,
+              "#f768a1", 6,
+              "#dd3497", 8,
+              "#ae017e", 10,
+              "#7a0177", 100,
+              "#000000"],
+            "line-width": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              12, 2.1,
+              14, 5.25,
+              15, 7.5,
+              16, 18,
+              18, 52.5,
+              22, 150
+            ],
+          }
+        });
+        
+        document.getElementById("rnetlegend").innerHTML = `<button onclick="show_rnet_legend(false)" style="float:right" aria-label="Hide legend"><i class="fas fa-times"></i></button>
+        <h4>Gradient</h4>
+        <div><span style="background-color: #feebe2;" class="legenddot"></span>0</div>
+    	  <div><span style="background-color: #fcc5c0;" class="legenddot"></span>2</div>
+    	  <div><span style="background-color: #fa9fb5;" class="legenddot"></span>4</div>
+    	  <div><span style="background-color: #f768a1;" class="legenddot"></span>6</div>
+    	  <div><span style="background-color: #dd3497;" class="legenddot"></span>8</div>
+    	  <div><span style="background-color: #ae017e;" class="legenddot"></span>10</div>
+    	  <div><span style="background-color: #7a0177;" class="legenddot"></span>100</div>`;
+        
         break;
       default:
         map.addLayer({
@@ -67,7 +108,7 @@ function switch_rnet() {
           'paint': {
             'line-color': ["step", ["get", layerId],
               "rgba(0,0,0,0)", 1,
-              "#9C9C9C", 0,
+              "#9C9C9C", 10,
               "#FFFF73", 50,
               "#AFFF00", 100,
               "#00FFFF", 250,
@@ -88,10 +129,8 @@ function switch_rnet() {
             ],
           }
         });
-    
-    }
-
-    document.getElementById("rnetlegend").innerHTML = `<button onclick="show_rnet_legend(false)" style="float:right" aria-label="Hide legend"><i class="fas fa-times"></i></button>
+        
+        document.getElementById("rnetlegend").innerHTML = `<button onclick="show_rnet_legend(false)" style="float:right" aria-label="Hide legend"><i class="fas fa-times"></i></button>
         <h4>Number of Cyclists</h4>
         <div><span style="background-color: #9C9C9C;" class="legenddot"></span>1-9</div>
     	  <div><span style="background-color: #FFFF73;" class="legenddot"></span>10-49</div>
@@ -101,6 +140,10 @@ function switch_rnet() {
     	  <div><span style="background-color: #2E5FFF;" class="legenddot"></span>500-999</div>
     	  <div><span style="background-color: #0000FF;" class="legenddot"></span>1000-1999</div>
     	  <div><span style="background-color: #FF00C5;" class="legenddot"></span>2000+</div>`;
+    
+    }
+
+    
   } else {
     document.getElementById("rnetlegend").innerHTML = ``;
     if (map.getLayer("rnet")) map.removeLayer("rnet");
