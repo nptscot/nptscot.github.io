c399f068b62615c05cc983601131ce2e44f8af6d
Malcolm Morgan <mem48@users.noreply.github.com>
2023-01-19 14:22:46 +0000

    Add help boxes and overlay

--- js/welcome.js
+++ js/welcome.js
@@ -1,29 +1,32 @@
-//function hidewelcome() {
-//  var x = document.getElementById("welcome");
-//  if (x.style.display === "none") {
-//    x.style.display = "block";
-//  } else {
-//    x.style.display = "none";
-//  }
-//  
-//} 
-
-// Get the welcome
-var welcome = document.getElementById("welcome");
-
-// Get the <span> element that closes the welcome
+// Get the <span> element that closes the welcome or help
 var span = document.getElementsByClassName("closewelcome")[0];
+var spanhelp = document.getElementsByClassName("closehelp")[0];
 
-// When the user clicks on <span> (x), close the welcome
+// When the user clicks on <span> (x), close the welcome and help
 span.onclick = function() {
-  welcome.style.display = "none";
+  document.getElementById("welcome").style.display = "none";
+  toggle_overlay(false);
+};
+
+spanhelp.onclick = function() {
+  document.getElementById("help").style.display = "none";
+  toggle_overlay(false);
 };
 
 // When the user clicks anywhere outside of the welcome, close it
-window.onclick = function(event) {
-  if (event.target == welcome) {
-	welcome.style.display = "none";
-  }
-};
+//window.onclick = function(event) {
+//  if(!welcome.contains(event.target)){
+//	welcome.style.display = "none";
+//	toggle_overlay(false);
+//  }
+//};
 
+function toggle_overlay(on) {
+  console.log(on)
+  if(on){
+    document.getElementById("overlay").style.display = "block";
+  } else {
+    document.getElementById("overlay").style.display = "none";
+  }
+}
 
