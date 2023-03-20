6c9003729eb2dcad26f49874d697e9a924459ecd
Malcolm Morgan <mem48@users.noreply.github.com>
2023-03-20 16:24:41 +0000

    missed one

--- js/ga_optout.js
+++ js/ga_optout.js
@@ -35,7 +35,7 @@ function setCookie(cvalue) {
   var d = new Date();
   d.setTime(d.getTime() + (1000 * 24 * 60 * 60 * 1000));
   var expires = "expires="+d.toUTCString();
-  document.cookie = "PBCCtrack=" + cvalue + ";" + expires + ";path=/";
+  document.cookie = "NTPtrack=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie() {
