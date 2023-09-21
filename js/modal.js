e06ceb78ce69c6a29d174d1879d71fd6ca941a5d
Malcolm Morgan <mem48@users.noreply.github.com>
2023-09-21 14:05:50 +0100

    Improved modals

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,8 @@
 // Declare Chart Values
-var mode_commute_chart;
+var commuteOriginChart;
+var commuteDestinationChart;
+var primaryOrginChart;
+var secondaryOriginChart;
 
 // Deinfe the modal
 
@@ -64,61 +67,200 @@ map.on('click', 'data_zones', function(e) {
 
 makeChartsModeshare = function(sub){
   
-  // Mode share by origin
-  // Mode share by desitination
-  // Baseline
-  // GoDuch
-  
-  // 4 stacke bar makeChartsTransport
-  // Travel to Work Modeshare
-	if(mode_commute_chart){
-		mode_commute_chart.destroy();
+	if(commuteOriginChart){
+		commuteOriginChart.destroy();
+	}
+	if(commuteDestinationChart){
+		commuteDestinationChart.destroy();
+	}
+	if(primaryOrginChart){
+		primaryOrginChart.destroy();
+	}
+	if(secondaryOriginChart){
+		secondaryOriginChart.destroy();
 	}
   
-  var bicycle = [
+  
+  // Commute Origin
+  var bicycle_comm_orig = [
     sub.comm_orig_bicycle, 
-    sub.comm_orig_bicycle_go_dutch_fastest, 
-    sub.comm_dest_bicycle, 
-    sub.comm_dest_bicycle_go_dutch_fastest
+    sub.comm_orig_bicycle_go_dutch_fastest,
+    sub.comm_orig_bicycle_ebike_fastest,
+    sub.comm_orig_bicycle_go_dutch_quietest,
+    sub.comm_orig_bicycle_ebike_quietest
     ];
   
-  var car = [
-    sub.comm_orig_car, 
-    sub.comm_orig_car_go_dutch_fastest, 
-    sub.comm_dest_car, 
-    sub.comm_dest_car_go_dutch_fastest
-    ];
-    
-  var public_transport = [
-    sub.comm_orig_public_transport, 
-    sub.comm_orig_public_transport_go_dutch_fastest, 
-    sub.comm_dest_public_transport, 
-    sub.comm_dest_public_transport_go_dutch_fastest
-    ];
-    
-  var foot = [
+  var foot_comm_orig = [
     sub.comm_orig_foot, 
-    sub.comm_orig_foot_go_dutch_fastest, 
-    sub.comm_dest_foot, 
-    sub.comm_dest_foot_go_dutch_fastest
+    sub.comm_orig_foot_go_dutch_fastest,
+    sub.comm_orig_foot_ebike_fastest,
+    sub.comm_orig_foot_go_dutch_quietest,
+    sub.comm_orig_foot_ebike_quietest
     ];
     
-  var taxi = [
+  var car_comm_orig = [
+    sub.comm_orig_car, 
+    sub.comm_orig_car_go_dutch_fastest,
+    sub.comm_orig_car_ebike_fastest,
+    sub.comm_orig_car_go_dutch_quietest,
+    sub.comm_orig_car_ebike_quietest
+    ];
+    
+  var public_transport_comm_orig = [
+    sub.comm_orig_public_transport, 
+    sub.comm_orig_public_transport_go_dutch_fastest,
+    sub.comm_orig_public_transport_ebike_fastest,
+    sub.comm_orig_public_transport_go_dutch_quietest,
+    sub.comm_orig_public_transport_ebike_quietest
+    ];
+    
+  var taxi_comm_orig = [
     sub.comm_orig_taxi, 
-    sub.comm_orig_taxi_go_dutch_fastest, 
-    sub.comm_dest_taxi, 
-    sub.comm_dest_taxi_go_dutch_fastest
+    sub.comm_orig_taxi_go_dutch_fastest,
+    sub.comm_orig_taxi_ebike_fastest,
+    sub.comm_orig_taxi_go_dutch_quietest,
+    sub.comm_orig_taxi_ebike_quietest
     ];
   
   
-  var mode_commute_ctx = document.getElementById('mode_commute_chart').getContext('2d');
-	mode_commute_chart = new Chart(mode_commute_ctx, {
+  // Commute Destination
+  var bicycle_comm_dest = [
+    sub.comm_dest_bicycle, 
+    sub.comm_dest_bicycle_go_dutch_fastest,
+    sub.comm_dest_bicycle_ebike_fastest,
+    sub.comm_dest_bicycle_go_dutch_quietest,
+    sub.comm_dest_bicycle_ebike_quietest
+    ];
+  
+  var foot_comm_dest = [
+    sub.comm_dest_foot, 
+    sub.comm_dest_foot_go_dutch_fastest,
+    sub.comm_dest_foot_ebike_fastest,
+    sub.comm_dest_foot_go_dutch_quietest,
+    sub.comm_dest_foot_ebike_quietest
+    ];
+    
+  var car_comm_dest = [
+    sub.comm_dest_car, 
+    sub.comm_dest_car_go_dutch_fastest,
+    sub.comm_dest_car_ebike_fastest,
+    sub.comm_dest_car_go_dutch_quietest,
+    sub.comm_dest_car_ebike_quietest
+    ];
+    
+  var public_transport_comm_dest = [
+    sub.comm_dest_public_transport, 
+    sub.comm_dest_public_transport_go_dutch_fastest,
+    sub.comm_dest_public_transport_ebike_fastest,
+    sub.comm_dest_public_transport_go_dutch_quietest,
+    sub.comm_dest_public_transport_ebike_quietest
+    ];
+    
+  var taxi_comm_dest = [
+    sub.comm_dest_taxi, 
+    sub.comm_dest_taxi_go_dutch_fastest,
+    sub.comm_dest_taxi_ebike_fastest,
+    sub.comm_dest_taxi_go_dutch_quietest,
+    sub.comm_dest_taxi_ebike_quietest
+    ];
+    
+  // Primary Orig
+  var bicycle_primary_orig = [
+    sub.bicycle_primary, 
+    sub.schl_orig_bicycle_go_dutch_primary_fastest,
+    sub.schl_orig_bicycle_ebike_primary_fastest,
+    sub.schl_orig_bicycle_go_dutch_primary_quietest,
+    sub.schl_orig_bicycle_ebike_primary_quietest
+    ];
+  
+  var foot_primary_orig = [
+    sub.foot_primary, 
+    sub.schl_orig_foot_go_dutch_primary_fastest,
+    sub.schl_orig_foot_ebike_primary_fastest,
+    sub.schl_orig_foot_go_dutch_primary_quietest,
+    sub.schl_orig_foot_ebike_primary_quietest
+    ];
+    
+  var car_primary_orig = [
+    sub.car_primary, 
+    sub.schl_orig_car_go_dutch_primary_fastest,
+    sub.schl_orig_car_ebike_primary_fastest,
+    sub.schl_orig_car_go_dutch_primary_quietest,
+    sub.schl_orig_car_ebike_primary_quietest
+    ];
+    
+  var public_transport_primary_orig = [
+    sub.public_transport_primary, 
+    sub.schl_orig_public_transport_go_dutch_primary_fastest,
+    sub.schl_orig_public_transport_ebike_primary_fastest,
+    sub.schl_orig_public_transport_go_dutch_primary_quietest,
+    sub.schl_orig_public_transport_ebike_primary_quietest
+    ];
+    
+  var other_primary_orig = [
+    sub.other_primary, 
+    sub.schl_orig_other_go_dutch_primary_fastest,
+    sub.schl_orig_other_ebike_primary_fastest,
+    sub.schl_orig_other_go_dutch_primary_quietest,
+    sub.schl_orig_other_ebike_primary_quietest
+    ];
+    
+  // Secondary Orig
+  var bicycle_secondary_orig = [
+    sub.bicycle_secondary, 
+    sub.schl_orig_bicycle_go_dutch_secondary_fastest,
+    sub.schl_orig_bicycle_ebike_secondary_fastest,
+    sub.schl_orig_bicycle_go_dutch_secondary_quietest,
+    sub.schl_orig_bicycle_ebike_secondary_quietest
+    ];
+  
+  var foot_secondary_orig = [
+    sub.foot_secondary, 
+    sub.schl_orig_foot_go_dutch_secondary_fastest,
+    sub.schl_orig_foot_ebike_secondary_fastest,
+    sub.schl_orig_foot_go_dutch_secondary_quietest,
+    sub.schl_orig_foot_ebike_secondary_quietest
+    ];
+    
+  var car_secondary_orig = [
+    sub.car_secondary, 
+    sub.schl_orig_car_go_dutch_secondary_fastest,
+    sub.schl_orig_car_ebike_secondary_fastest,
+    sub.schl_orig_car_go_dutch_secondary_quietest,
+    sub.schl_orig_car_ebike_secondary_quietest
+    ];
+    
+  var public_transport_secondary_orig = [
+    sub.public_transport_secondary, 
+    sub.schl_orig_public_transport_go_dutch_secondary_fastest,
+    sub.schl_orig_public_transport_ebike_secondary_fastest,
+    sub.schl_orig_public_transport_go_dutch_secondary_quietest,
+    sub.schl_orig_public_transport_ebike_secondary_quietest
+    ];
+    
+  var other_secondary_orig = [
+    sub.other_secondary, 
+    sub.schl_orig_other_go_dutch_secondary_fastest,
+    sub.schl_orig_other_ebike_secondary_fastest,
+    sub.schl_orig_other_go_dutch_secondary_quietest,
+    sub.schl_orig_other_ebike_secondary_quietest
+    ];
+  
+
+  var commuteOrigin_ctx = document.getElementById('commuteOriginChart').getContext('2d');
+  var commuteDestination_ctx = document.getElementById('commuteDestinationChart').getContext('2d');
+  var primaryOrgin_ctx = document.getElementById('primaryOrginChart').getContext('2d');
+  var secondaryOrigin_ctx = document.getElementById('secondaryOriginChart').getContext('2d');
+  
+  
+  
+	commuteOriginChart = new Chart(commuteOrigin_ctx, {
 		type: 'bar',
 		data: {
-			labels: ['Leaving Baseline','Leaving Go Dutch','Arriving Baseline','Arriving Go Dutch'],
+			labels: ['Baseline','Go Dutch (Fastest)','Ebike (Fastest)','Go Dutch (Quietest)','Ebike (Quietest)'],
 			datasets: [{
 				label: 'Bicycle',
-				data: bicycle,
+				data: bicycle_comm_orig,
 				backgroundColor: 'rgba(51,160,44, 0.8)',
 				borderColor: 'rgba(51,160,44, 1)',
 				borderWidth: 1,
@@ -126,7 +268,7 @@ makeChartsModeshare = function(sub){
 			},
 			{
 				label: 'Car',
-				data: car,
+				data: car_comm_orig,
 				backgroundColor: 'rgba(227,26,28, 0.8)',
 				borderColor: 'rgba(227,26,28, 1)',
 				borderWidth: 1,
@@ -134,7 +276,7 @@ makeChartsModeshare = function(sub){
 			},
 			{
 				label: 'Public transport',
-				data: public_transport ,
+				data: public_transport_comm_orig ,
 				backgroundColor: 'rgba(56,108,176, 0.8)',
 				borderColor: 'rgba(56,108,176, 1)',
 				borderWidth: 1,
@@ -142,7 +284,7 @@ makeChartsModeshare = function(sub){
 			},
 			{
 				label: 'Foot',
-				data: foot,
+				data: foot_comm_orig,
 				backgroundColor: 'rgba(178,223,138, 0.8)',
 				borderColor: 'rgba(178,223,138, 1)',
 				borderWidth: 1,
@@ -150,7 +292,7 @@ makeChartsModeshare = function(sub){
 			},
 			{
 				label: 'Taxi',
-				data: taxi,
+				data: taxi_comm_orig,
 				backgroundColor: 'rgba(166,206,227, 0.8)',
 				borderColor: 'rgba(166,206,227, 1)',
 				borderWidth: 1,
@@ -181,11 +323,212 @@ makeChartsModeshare = function(sub){
 		}
 	});
   
-  
+  commuteDestinationChart = new Chart(commuteDestination_ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Baseline','Go Dutch (Fastest)','Ebike (Fastest)','Go Dutch (Quietest)','Ebike (Quietest)'],
+			datasets: [{
+				label: 'Bicycle',
+				data: bicycle_comm_dest,
+				backgroundColor: 'rgba(51,160,44, 0.8)',
+				borderColor: 'rgba(51,160,44, 1)',
+				borderWidth: 1,
+				order: 1
+			},
+			{
+				label: 'Car',
+				data: car_comm_dest,
+				backgroundColor: 'rgba(227,26,28, 0.8)',
+				borderColor: 'rgba(227,26,28, 1)',
+				borderWidth: 1,
+				order: 5
+			},
+			{
+				label: 'Public transport',
+				data: public_transport_comm_dest ,
+				backgroundColor: 'rgba(56,108,176, 0.8)',
+				borderColor: 'rgba(56,108,176, 1)',
+				borderWidth: 1,
+				order: 3
+			},
+			{
+				label: 'Foot',
+				data: foot_comm_dest,
+				backgroundColor: 'rgba(178,223,138, 0.8)',
+				borderColor: 'rgba(178,223,138, 1)',
+				borderWidth: 1,
+				order: 2
+			},
+			{
+				label: 'Taxi',
+				data: taxi_comm_dest,
+				backgroundColor: 'rgba(166,206,227, 0.8)',
+				borderColor: 'rgba(166,206,227, 1)',
+				borderWidth: 1,
+				order: 6
+			}
+			
+			]
+		},
+		options: {
+			scales: {
+				y: {
+				  stacked: true,
+				  scaleLabel: {
+            display: true,
+            labelString: 'Daily commuters'
+          },
+					ticks: {
+						beginAtZero: true,
+						
+					}
+				},
+				x: {
+				  stacked: true
+				},
+			},
+			responsive: true,
+			maintainAspectRatio: false
+		}
+	});
 	
+	primaryOrginChart = new Chart(primaryOrgin_ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Baseline','Go Dutch (Fastest)','Ebike (Fastest)','Go Dutch (Quietest)','Ebike (Quietest)'],
+			datasets: [{
+				label: 'Bicycle',
+				data: bicycle_primary_orig,
+				backgroundColor: 'rgba(51,160,44, 0.8)',
+				borderColor: 'rgba(51,160,44, 1)',
+				borderWidth: 1,
+				order: 1
+			},
+			{
+				label: 'Car',
+				data: car_primary_orig,
+				backgroundColor: 'rgba(227,26,28, 0.8)',
+				borderColor: 'rgba(227,26,28, 1)',
+				borderWidth: 1,
+				order: 5
+			},
+			{
+				label: 'Public transport',
+				data: public_transport_primary_orig ,
+				backgroundColor: 'rgba(56,108,176, 0.8)',
+				borderColor: 'rgba(56,108,176, 1)',
+				borderWidth: 1,
+				order: 3
+			},
+			{
+				label: 'Foot',
+				data: foot_primary_orig,
+				backgroundColor: 'rgba(178,223,138, 0.8)',
+				borderColor: 'rgba(178,223,138, 1)',
+				borderWidth: 1,
+				order: 2
+			},
+			{
+				label: 'Other',
+				data: other_primary_orig,
+				backgroundColor: 'rgba(166,206,227, 0.8)',
+				borderColor: 'rgba(166,206,227, 1)',
+				borderWidth: 1,
+				order: 6
+			}
+			
+			]
+		},
+		options: {
+			scales: {
+				y: {
+				  stacked: true,
+				  scaleLabel: {
+            display: true,
+            labelString: 'Daily commuters'
+          },
+					ticks: {
+						beginAtZero: true,
+						
+					}
+				},
+				x: {
+				  stacked: true
+				},
+			},
+			responsive: true,
+			maintainAspectRatio: false
+		}
+	});
 	
-	
-	
+	secondaryOriginChart = new Chart(secondaryOrigin_ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Baseline','Go Dutch (Fastest)','Ebike (Fastest)','Go Dutch (Quietest)','Ebike (Quietest)'],
+			datasets: [{
+				label: 'Bicycle',
+				data: bicycle_secondary_orig,
+				backgroundColor: 'rgba(51,160,44, 0.8)',
+				borderColor: 'rgba(51,160,44, 1)',
+				borderWidth: 1,
+				order: 1
+			},
+			{
+				label: 'Car',
+				data: car_secondary_orig,
+				backgroundColor: 'rgba(227,26,28, 0.8)',
+				borderColor: 'rgba(227,26,28, 1)',
+				borderWidth: 1,
+				order: 5
+			},
+			{
+				label: 'Public transport',
+				data: public_transport_secondary_orig ,
+				backgroundColor: 'rgba(56,108,176, 0.8)',
+				borderColor: 'rgba(56,108,176, 1)',
+				borderWidth: 1,
+				order: 3
+			},
+			{
+				label: 'Foot',
+				data: foot_secondary_orig,
+				backgroundColor: 'rgba(178,223,138, 0.8)',
+				borderColor: 'rgba(178,223,138, 1)',
+				borderWidth: 1,
+				order: 2
+			},
+			{
+				label: 'Other',
+				data: other_secondary_orig,
+				backgroundColor: 'rgba(166,206,227, 0.8)',
+				borderColor: 'rgba(166,206,227, 1)',
+				borderWidth: 1,
+				order: 6
+			}
+			
+			]
+		},
+		options: {
+			scales: {
+				y: {
+				  stacked: true,
+				  scaleLabel: {
+            display: true,
+            labelString: 'Daily commuters'
+          },
+					ticks: {
+						beginAtZero: true,
+						
+					}
+				},
+				x: {
+				  stacked: true
+				},
+			},
+			responsive: true,
+			maintainAspectRatio: false
+		}
+	});
 	
 	
 	
--- js/modal_school.js
+++ js/modal_school.js
@@ -1,6 +1,6 @@
 // Declare Chart Values
-var t2sChart;
-
+var primaryChart;
+var secondaryChart;
 // Deinfe the modal
 
 var school_modal = document.getElementById("school_modal");
@@ -66,89 +66,248 @@ map.on('click', 'schools', function(e) {
 
 makeChartsModeshareSchool = function(sub){
   
-  // Mode share by origin
-  // Mode share by desitination
-  // Baseline
-  // GoDuch
-  
-  // 4 stacke bar makeChartsTransport
-  // Travel to Work Modeshare
-	if(t2sChart){
-		t2sChart.destroy();
-	}
-	
-	var currentShare = [
+	var bicycle_primary = [
 	  sub.bicycle_primary,
-	  sub.foot_primary,
-	  sub.public_transport_primary,
-	  sub.car_primary,
-	  sub.other_primary
-	];
-	
-	
-	var futureShare = [
 	  sub.bicycle_go_dutch_primary_fastest,
-	  sub.foot_go_dutch_primary_fastest,
-	  sub.public_transport_go_dutch_primary_fastest,
-	  sub.car_go_dutch_primary_fastest,
-	  sub.other_go_dutch_primary_fastest
+	  sub.bicycle_ebike_primary_fastest,
+	  sub.bicycle_go_dutch_primary_quietest,
+	  sub.bicycle_ebike_primary_quietest
 	];
+	
+	var foot_primary = [
+	  sub.foot_primary,
+	  sub.foot_go_dutch_primary_fastest,
+	  sub.foot_ebike_primary_fastest,
+	  sub.foot_go_dutch_primary_quietest,
+	  sub.foot_ebike_primary_quietest
+	];
+	
+	var car_primary = [
+	  sub.car_primary,
+	  sub.car_go_dutch_primary_fastest,
+	  sub.car_ebike_primary_fastest,
+	  sub.car_go_dutch_primary_quietest,
+	  sub.car_ebike_primary_quietest
+	];
+	
+	var public_transport_primary = [
+	  sub.public_transport_primary,
+	  sub.public_transport_go_dutch_primary_fastest,
+	  sub.public_transport_ebike_primary_fastest,
+	  sub.public_transport_go_dutch_primary_quietest,
+	  sub.public_transport_ebike_primary_quietest
+	];
+	
+	var other_primary = [
+	  sub.other_primary,
+	  sub.other_go_dutch_primary_fastest,
+	  sub.other_ebike_primary_fastest,
+	  sub.other_go_dutch_primary_quietest,
+	  sub.other_ebike_primary_quietest
+	];
+
+	
+	var bicycle_secondary = [
+	  sub.bicycle_secondary,
+    sub.bicycle_go_dutch_secondary_fastest,
+    sub.bicycle_ebike_secondary_fastest,
+    sub.bicycle_go_dutch_secondary_quietest,
+    sub.bicycle_ebike_secondary_quietest
+	];
+	
+	var foot_secondary = [
+	  sub.foot_secondary,
+    sub.foot_go_dutch_secondary_fastest,
+    sub.foot_ebike_secondary_fastest,
+    sub.foot_go_dutch_secondary_quietest,
+    sub.foot_ebike_secondary_quietest
+	];
+	
+	var car_secondary = [
+	  sub.car_secondary,
+    sub.car_go_dutch_secondary_fastest,
+    sub.car_ebike_secondary_fastest,
+    sub.car_go_dutch_secondary_quietest,
+    sub.car_ebike_secondary_quietest
+	];
+	
+	var public_transport_secondary = [
+	  sub.public_transport_secondary,
+    sub.public_transport_go_dutch_secondary_fastest,
+    sub.public_transport_ebike_secondary_fastest,
+    sub.public_transport_go_dutch_secondary_quietest,
+    sub.public_transport_ebike_secondary_quietest
+	];
+	
+	var other_secondary = [
+	  sub.other_secondary,
+    sub.other_go_dutch_secondary_fastest,
+    sub.other_ebike_secondary_fastest,
+    sub.other_go_dutch_secondary_quietest,
+    sub.other_ebike_secondary_quietest
+	];
+	
   
   
   // Travel to School Modeshare
-	if(t2sChart){
-		t2sChart.destroy();
+	if(primaryChart){
+		primaryChart.destroy();
 	}
 	
-	var t2sctx = document.getElementById('t2sChart').getContext('2d');
-	t2sChart = new Chart(t2sctx, {
-		type: 'doughnut',
+	if(secondaryChart){
+		secondaryChart.destroy();
+	}
+	
+	var primaryctx = document.getElementById('primaryChart').getContext('2d');
+	var secondaryctx = document.getElementById('secondaryChart').getContext('2d');
+	
+	primaryChart = new Chart(primaryctx, {
+		type: 'bar',
 		data: {
+			labels: ['Baseline',
+			         'Go Dutch (Fastest)','Ebike (Fastest)',
+			         'Go Dutch (Quietest)','Ebike (Quietest)'],
 			datasets: [{
-				label: 'Go Dutch',
-				data: futureShare,
-				backgroundColor: [
-				'rgba(178,223,138, 1)',
-				'rgba(51,160,44, 1)',
-				'rgba(227,26,28, 1)',
-				'rgba(128,128,128, 1)',
-				'rgba(35,35,35, 1)'
-				],
-				weight: 0.2
-				
-			},{
-				label: 'Basline',
-				data: currentShare,
-				backgroundColor: [
-				'rgba(178,223,138, 1)',
-				'rgba(51,160,44, 1)',
-				'rgba(227,26,28, 1)',
-				'rgba(128,128,128, 1)',
-				'rgba(35,35,35, 1)'
-				]
-				
-			}],
+				label: 'Bicycle',
+				data: bicycle_primary,
+				backgroundColor: 'rgba(51,160,44, 0.8)',
+				borderColor: 'rgba(51,160,44, 1)',
+				borderWidth: 1,
+				order: 1
+			},
+			{
+				label: 'Car',
+				data: car_primary,
+				backgroundColor: 'rgba(227,26,28, 0.8)',
+				borderColor: 'rgba(227,26,28, 1)',
+				borderWidth: 1,
+				order: 5
+			},
+			{
+				label: 'Public transport',
+				data: public_transport_primary ,
+				backgroundColor: 'rgba(56,108,176, 0.8)',
+				borderColor: 'rgba(56,108,176, 1)',
+				borderWidth: 1,
+				order: 3
+			},
+			{
+				label: 'Foot',
+				data: foot_primary,
+				backgroundColor: 'rgba(178,223,138, 0.8)',
+				borderColor: 'rgba(178,223,138, 1)',
+				borderWidth: 1,
+				order: 2
+			},
+			{
+				label: 'Other',
+				data: other_primary,
+				backgroundColor: 'rgba(166,206,227, 0.8)',
+				borderColor: 'rgba(166,206,227, 1)',
+				borderWidth: 1,
+				order: 6
+			}
 			
-			labels: ['Bicycle','Foot','Public Transport','Car','Other']
+			]
 		},
 		options: {
+			scales: {
+				y: {
+				  stacked: true,
+				  title: {
+            display: true,
+            text: 'Number of Children'
+          },
+				  scaleLabel: {
+            display: true
+          },
+					ticks: {
+						beginAtZero: true,
+						
+					}
+				},
+				x: {
+				  stacked: true
+				},
+			},
 			responsive: true,
-			maintainAspectRatio: false,
-			cutoutPercentage: 0,
-			legend : {
-			  position: 'right',
-        align: 'middle'
-			}
+			maintainAspectRatio: false
 		}
 	});
-  
-  
 	
-	
-	
-	
-	
-	
+	secondaryChart = new Chart(secondaryctx, {
+		type: 'bar',
+		data: {
+			labels: ['Baseline',
+			         'Go Dutch (Fastest)','Ebike (Fastest)',
+			         'Go Dutch (Quietest)','Ebike (Quietest)'],
+			datasets: [{
+				label: 'Bicycle',
+				data: bicycle_secondary,
+				backgroundColor: 'rgba(51,160,44, 0.8)',
+				borderColor: 'rgba(51,160,44, 1)',
+				borderWidth: 1,
+				order: 1
+			},
+			{
+				label: 'Car',
+				data: car_secondary,
+				backgroundColor: 'rgba(227,26,28, 0.8)',
+				borderColor: 'rgba(227,26,28, 1)',
+				borderWidth: 1,
+				order: 5
+			},
+			{
+				label: 'Public transport',
+				data: public_transport_secondary ,
+				backgroundColor: 'rgba(56,108,176, 0.8)',
+				borderColor: 'rgba(56,108,176, 1)',
+				borderWidth: 1,
+				order: 3
+			},
+			{
+				label: 'Foot',
+				data: foot_secondary,
+				backgroundColor: 'rgba(178,223,138, 0.8)',
+				borderColor: 'rgba(178,223,138, 1)',
+				borderWidth: 1,
+				order: 2
+			},
+			{
+				label: 'Other',
+				data: other_secondary,
+				backgroundColor: 'rgba(166,206,227, 0.8)',
+				borderColor: 'rgba(166,206,227, 1)',
+				borderWidth: 1,
+				order: 6
+			}
+			
+			]
+		},
+		options: {
+			scales: {
+				y: {
+				  stacked: true,
+				  title: {
+            display: true,
+            text: 'Number of Children'
+          },
+				  scaleLabel: {
+            display: true
+          },
+					ticks: {
+						beginAtZero: true,
+						
+					}
+				},
+				x: {
+				  stacked: true
+				},
+			},
+			responsive: true,
+			maintainAspectRatio: false
+		}
+	});
 	
 };
 
