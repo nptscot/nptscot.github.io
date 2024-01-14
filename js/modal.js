79fe527635cd22824fe37c8f74f87e0da78b752a
mvl22 <martin@lucas-smith.co.uk>
2024-01-14 19:42:51 +0000

    Charts: Reformat code (no logic changes)

--- js/modal.js
+++ js/modal.js
@@ -1,190 +1,174 @@
 // Data zones
 
-dataZonesModal = function ()
-{
+dataZonesModal = function () {
 
-var chartDefinitions = {
-// #!# Labels are sometimes wrong, but don't seem to be used anyway
-	charts: [
-		['commuteOriginChart', 'comm_orig', 'Daily commuters'],			// Commute Origin
-		['commuteDestinationChart', 'comm_dest', 'Daily commuters'],// Commute Destination
-		['primaryOrginChart', 'schl_primary_orig', 'Daily commuters'],	// School Primary Origin
-		['secondaryOriginChart', 'schl_secondary_orig', 'Daily commuters'],	// School Secondary Origin
-		['shoppingOriginChart', 'shopping_orig', 'Daily shoppers'],	// shopping Origin
-		['shoppingDestinationChart', 'shopping_dest', 'Daily shoppers'],	// shopping Destination
-		['leisureOriginChart', 'leisure_orig', 'Daily shoppers'],// leisure Origin
-		['leisureDestinationChart', 'leisure_dest', 'Daily shoppers'],	// leisure Destination
-		['visitingOriginChart', 'visiting_orig', 'Daily shoppers'],	// visiting Origin
-		['visitingDestinationChart', 'visiting_dest', 'Daily shoppers'],	// visiting Destination
-	],
-	
-	modes: [
+	var chartDefinitions = {
+	  // #!# Labels are sometimes wrong, but don't seem to be used anyway
+	  charts: [
+		['commuteOriginChart', 'comm_orig', 'Daily commuters'], // Commute Origin
+		['commuteDestinationChart', 'comm_dest', 'Daily commuters'], // Commute Destination
+		['primaryOrginChart', 'schl_primary_orig', 'Daily commuters'], // School Primary Origin
+		['secondaryOriginChart', 'schl_secondary_orig', 'Daily commuters'], // School Secondary Origin
+		['shoppingOriginChart', 'shopping_orig', 'Daily shoppers'], // shopping Origin
+		['shoppingDestinationChart', 'shopping_dest', 'Daily shoppers'], // shopping Destination
+		['leisureOriginChart', 'leisure_orig', 'Daily shoppers'], // leisure Origin
+		['leisureDestinationChart', 'leisure_dest', 'Daily shoppers'], // leisure Destination
+		['visitingOriginChart', 'visiting_orig', 'Daily shoppers'], // visiting Origin
+		['visitingDestinationChart', 'visiting_dest', 'Daily shoppers'], // visiting Destination
+	  ],
+  
+	  modes: [
 		// Label, field (e.g. bicycle => comm_orig_bicycle_ebike_fastest), background colour, border colour
 		['Bicycle', 'bicycle', 'rgba(51,160,44, 0.8)', 'rgba(51,160,44, 1)'],
 		['Foot', 'foot', 'rgba(178,223,138, 0.8)', 'rgba(178,223,138, 1)'],
 		['Public transport', 'public_transport', 'rgba(56,108,176, 0.8)', 'rgba(56,108,176, 1)'],
 		['Car', 'car', 'rgba(227,26,28, 0.8)', 'rgba(227,26,28, 1)'],
 		['Taxi', 'taxi', 'rgba(166,206,227, 0.8)', 'rgba(166,206,227, 1)'],
-	],
-	
-	suffixes: [
-		'', 
+	  ],
+  
+	  suffixes: [
+		'',
 		'_go_dutch_fastest',
 		'_ebike_fastest',
 		'_go_dutch_quietest',
 		'_ebike_quietest'
-	],
-	
-	labels: [
+	  ],
+  
+	  labels: [
 		'Baseline',
 		'Go Dutch (Fastest)',
 		'Ebike (Fastest)',
 		'Go Dutch (Quietest)',
 		'Ebike (Quietest)'
-  	]
-};
-
-
-// Declare charts collection
-var charts = {};
-
-
-
-
-// Define the modal
-
-var zone_modal = document.getElementById("zone_modal");
-// Get the <span> element that closes the modal
-var span_modal = document.getElementsByClassName("closemodal")[0];
-
-// When the user clicks on <span> (x), close the modal
-span_modal.onclick = function() {
-  zone_modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == zone_modal) {
-	zone_modal.style.display = "none";
-  }
-};
-
-
-// How map triggers the modal 
-// On click open modal
-map.on('click', 'data_zones', function(e) {
-  
-  console.log("Click on Zones")
-  
-  // Block Modal when clicking on other layers
-  let f = map.queryRenderedFeatures(e.point);
-  var layersToExclude = ['composite', 'dasymetric','placenames'];
-
-  f = f.filter(function (el) {
-    return !layersToExclude.includes(el.source);
-  });
-  
-  if (f[0].sourceLayer == "data_zones") {
-    
-    zone_modal.style.display = "block";
-	
-    var sub = e.features[0].properties;
-  	var dataurl = 'https://nptscot.blob.core.windows.net/json/DataZone/' + sub.DataZone + '.json';
-    var zonedata;
-    $.getJSON(dataurl, function (json) {
-        console.log( "downloaded zone json" );
-        zonedata = json[0];
-    })
-      .done(function() {
-        document.getElementById("zone-modal-title").innerHTML = "<h2>Zone Summary: " + sub.DataZone + "</h2>";
-        // Define Charts
-  		  makeChartsModeshare(zonedata);
-      })
-      .fail(function() {
-        alert("Failed to get data for this zone, please try refreshing the page");
-      });
-    
-    //return;
-  } 
-	
-});
-
-
-
-
-
-
-makeChartsModeshare = function(sub){
-	
-  // Clear existing if present
-  Object.keys (charts).forEach (i => {
-    if(charts[i]) {
-		charts[i].destroy();
-	}
-  });
-  
-  function createArray(prefix, suffixes) {
-    return suffixes.map(suffix => sub[prefix + suffix]);
-  }
+	  ]
+	};
   
   
-  function createChart (id, prefix, labelString)
-  {
-	// Assemble the datasets
-	var datasets = [];
-	chartDefinitions.modes.forEach (mode => {
-		datasets.push (
-			{
-				label: mode[0],
-				data: createArray(prefix + '_' + mode[1], chartDefinitions.suffixes),
-				backgroundColor: mode[2],
-				borderColor: mode[3],
-				borderWidth: 1
-			}
-		)
+	// Declare charts collection
+	var charts = {};
+  
+  
+	// Define the modal
+  
+	var zone_modal = document.getElementById("zone_modal");
+	// Get the <span> element that closes the modal
+	var span_modal = document.getElementsByClassName("closemodal")[0];
+  
+	// When the user clicks on <span> (x), close the modal
+	span_modal.onclick = function () {
+	  zone_modal.style.display = "none";
+	};
+  
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+	  if (event.target == zone_modal) {
+		zone_modal.style.display = "none";
+	  }
+	};
+  
+  
+	// How map triggers the modal 
+	// On click open modal
+	map.on('click', 'data_zones', function (e) {
+  
+	  console.log("Click on Zones")
+  
+	  // Block Modal when clicking on other layers
+	  let f = map.queryRenderedFeatures(e.point);
+	  var layersToExclude = ['composite', 'dasymetric', 'placenames'];
+  
+	  f = f.filter(function (el) {
+		return !layersToExclude.includes(el.source);
+	  });
+  
+	  if (f[0].sourceLayer == "data_zones") {
+  
+		zone_modal.style.display = "block";
+  
+		var sub = e.features[0].properties;
+		var dataurl = 'https://nptscot.blob.core.windows.net/json/DataZone/' + sub.DataZone + '.json';
+		var zonedata;
+		$.getJSON(dataurl, function (json) {
+			console.log("downloaded zone json");
+			zonedata = json[0];
+		  })
+		  .done(function () {
+			document.getElementById("zone-modal-title").innerHTML = "<h2>Zone Summary: " + sub.DataZone + "</h2>";
+			// Define Charts
+			makeChartsModeshare(zonedata);
+		  })
+		  .fail(function () {
+			alert("Failed to get data for this zone, please try refreshing the page");
+		  });
+  
+		//return;
+	  }
+  
 	});
-	
   
-	return new Chart(document.getElementById(id).getContext('2d'), {
-		type: 'bar',
-		data: {
+  
+	makeChartsModeshare = function (sub) {
+  
+	  // Clear existing if present
+	  Object.keys(charts).forEach(i => {
+		if (charts[i]) {
+		  charts[i].destroy();
+		}
+	  });
+  
+	  function createArray(prefix, suffixes) {
+		return suffixes.map(suffix => sub[prefix + suffix]);
+	  }
+  
+  
+	  function createChart(id, prefix, labelString) {
+		// Assemble the datasets
+		var datasets = [];
+		chartDefinitions.modes.forEach(mode => {
+		  datasets.push({
+			label: mode[0],
+			data: createArray(prefix + '_' + mode[1], chartDefinitions.suffixes),
+			backgroundColor: mode[2],
+			borderColor: mode[3],
+			borderWidth: 1
+		  })
+		});
+  
+  
+		return new Chart(document.getElementById(id).getContext('2d'), {
+		  type: 'bar',
+		  data: {
 			labels: chartDefinitions.labels,
 			datasets: datasets
-		},
-		options: {
+		  },
+		  options: {
 			scales: {
-				y: {
-				  stacked: true,
-				  scaleLabel: {
-            display: true,
-            labelString: labelString
-          },
-					ticks: {
-						beginAtZero: true,
-						
-					}
+			  y: {
+				stacked: true,
+				scaleLabel: {
+				  display: true,
+				  labelString: labelString
 				},
-				x: {
-				  stacked: true
-				},
+				ticks: {
+				  beginAtZero: true,
+  
+				}
+			  },
+			  x: {
+				stacked: true
+			  },
 			},
 			responsive: true,
 			maintainAspectRatio: false
-		}
-	});
+		  }
+		});
+	  }
+  
+	  chartDefinitions.charts.forEach((chartDefinition, i) => {
+		charts[i] = createChart(chartDefinition[0], chartDefinition[1], chartDefinition[2]);
+	  });
+	};
+  
   }
   
-  
-  
-  chartDefinitions.charts.forEach ((chartDefinition, i) => {
-	charts[i] = createChart (chartDefinition[0], chartDefinition[1], chartDefinition[2]);
-  });
-  
-  
-  
-
-};
-
-}
-
-dataZonesModal ();
+  dataZonesModal();
--- js/modal_school.js
+++ js/modal_school.js
@@ -1,178 +1,170 @@
 // Travel to School Modeshare
 
-schoolsModal = function ()
-{
-	
-var chartDefinitions = {
-	
-	charts: [
-		['primaryChart', 'schl_primary_dest', 'Number of Children'],	// School Primary Destination
-		['secondaryChart', 'schl_primary_dest', 'Number of Children'],	// School Secondary Destination  #!# Data doesn't seem to be present/showing
-	],
-	
-	modes: [
+schoolsModal = function () {
+
+	var chartDefinitions = {
+  
+	  charts: [
+		['primaryChart', 'schl_primary_dest', 'Number of Children'], // School Primary Destination
+		['secondaryChart', 'schl_primary_dest', 'Number of Children'], // School Secondary Destination  #!# Data doesn't seem to be present/showing
+	  ],
+  
+	  modes: [
 		// Label, field (e.g. bicycle => comm_orig_bicycle_ebike_fastest), background colour, border colour
 		['Bicycle', 'bicycle', 'rgba(51,160,44, 0.8)', 'rgba(51,160,44, 1)'],
 		['Foot', 'foot', 'rgba(178,223,138, 0.8)', 'rgba(178,223,138, 1)'],
 		['Public transport', 'public_transport', 'rgba(56,108,176, 0.8)', 'rgba(56,108,176, 1)'],
 		['Car', 'car', 'rgba(227,26,28, 0.8)', 'rgba(227,26,28, 1)'],
-		['Other', 'other', 'rgba(166,206,227, 0.8)', 'rgba(166,206,227, 1)'],	// #!# NB the main modal has taxi rather than other
-	],
-	
-	suffixes: [
-		'', 
+		['Other', 'other', 'rgba(166,206,227, 0.8)', 'rgba(166,206,227, 1)'], // #!# NB the main modal has taxi rather than other
+	  ],
+  
+	  suffixes: [
+		'',
 		'_go_dutch_fastest',
 		'_ebike_fastest',
 		'_go_dutch_quietest',
 		'_ebike_quietest'
-	],
-	
-	labels: [
+	  ],
+  
+	  labels: [
 		'Baseline',
 		'Go Dutch (Fastest)',
 		'Ebike (Fastest)',
 		'Go Dutch (Quietest)',
 		'Ebike (Quietest)'
-	]
-};
-
-
-// Declare charts collection
-var charts = {};
-
-// Define the modal
-var school_modal = document.getElementById("school_modal");
-// Get the <span> element that closes the modal
-var span_modal = document.getElementsByClassName("closeschoolmodal")[0];
-
-// When the user clicks on <span> (x), close the modal
-span_modal.onclick = function() {
-  school_modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == school_modal) {
-	school_modal.style.display = "none";
-  }
-};
-
-// How map triggers the modal 
-// On click open modal
-map.on('click', 'schools', function(e) {
-  
-  console.log("Click on schools")
-  
-  // Block Modal when clicking on other layers
-  let f = map.queryRenderedFeatures(e.point);
-  var layersToExclude = ['composite', 'dasymetric','placenames'];
-
-  f = f.filter(function (el) {
-    return !layersToExclude.includes(el.source);
-    //return el.source != 'composite';
-  });
-  
-  //console.log(f[0].sourceLayer)
+	  ]
+	};
   
   
-  if (f[0].sourceLayer == "schools") {
-    
-    school_modal.style.display = "block";
-	
-    var sub = e.features[0].properties;
-  	var dataurl = 'https://nptscot.blob.core.windows.net/json/School/' + sub.SeedCode + '.json';
-    var schooldata;
-    $.getJSON(dataurl, function (json) {
-        console.log( "downloaded school json" );
-        schooldata = json[0];
-    })
-      .done(function() {
-        //Hide Spinner
-        //$('#loader').hide();
-        document.getElementById("school-modal-title").innerHTML = "<h2>" + sub.SchoolName + "</h2>";
-        // Define Charts
-  		  makeChartsModeshareSchool(schooldata);
-      })
-      .fail(function() {
-        alert("Failed to get data for this school, please try refreshing the page");
-      });
-    
-    //return;
-  } 
-	
-});
-
-
-
-makeChartsModeshareSchool = function(sub){
+	// Declare charts collection
+	var charts = {};
   
-	// Clear existing if present
-	Object.keys (charts).forEach (chartId => {
-		if(charts[chartId]) {
-			charts[chartId].destroy();
+	// Define the modal
+	var school_modal = document.getElementById("school_modal");
+	// Get the <span> element that closes the modal
+	var span_modal = document.getElementsByClassName("closeschoolmodal")[0];
+  
+	// When the user clicks on <span> (x), close the modal
+	span_modal.onclick = function () {
+	  school_modal.style.display = "none";
+	};
+  
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+	  if (event.target == school_modal) {
+		school_modal.style.display = "none";
+	  }
+	};
+  
+	// How map triggers the modal 
+	// On click open modal
+	map.on('click', 'schools', function (e) {
+  
+	  console.log("Click on schools")
+  
+	  // Block Modal when clicking on other layers
+	  let f = map.queryRenderedFeatures(e.point);
+	  var layersToExclude = ['composite', 'dasymetric', 'placenames'];
+  
+	  f = f.filter(function (el) {
+		return !layersToExclude.includes(el.source);
+		//return el.source != 'composite';
+	  });
+  
+	  //console.log(f[0].sourceLayer)
+  
+  
+	  if (f[0].sourceLayer == "schools") {
+  
+		school_modal.style.display = "block";
+  
+		var sub = e.features[0].properties;
+		var dataurl = 'https://nptscot.blob.core.windows.net/json/School/' + sub.SeedCode + '.json';
+		var schooldata;
+		$.getJSON(dataurl, function (json) {
+			console.log("downloaded school json");
+			schooldata = json[0];
+		  })
+		  .done(function () {
+			//Hide Spinner
+			//$('#loader').hide();
+			document.getElementById("school-modal-title").innerHTML = "<h2>" + sub.SchoolName + "</h2>";
+			// Define Charts
+			makeChartsModeshareSchool(schooldata);
+		  })
+		  .fail(function () {
+			alert("Failed to get data for this school, please try refreshing the page");
+		  });
+  
+		//return;
+	  }
+  
+	});
+  
+  
+	makeChartsModeshareSchool = function (sub) {
+  
+	  // Clear existing if present
+	  Object.keys(charts).forEach(chartId => {
+		if (charts[chartId]) {
+		  charts[chartId].destroy();
 		}
 	  });
-	
-  function createArray(prefix, suffixes) {
-    return suffixes.map(suffix => sub[prefix + suffix]);
-  }
   
-  function createChart (id, prefix, labelString)
-  {
-	// Assemble the datasets
-	var datasets = [];
-	chartDefinitions.modes.forEach (mode => {
-		datasets.push (
-			{
-				label: mode[0],
-				data: createArray(prefix + '_' + mode[1], chartDefinitions.suffixes),
-				backgroundColor: mode[2],
-				borderColor: mode[3],
-				borderWidth: 1
-			}
-		)
-	});
-	
-	return new Chart(document.getElementById(id).getContext('2d'), {
-		type: 'bar',
-		data: {
+	  function createArray(prefix, suffixes) {
+		return suffixes.map(suffix => sub[prefix + suffix]);
+	  }
+  
+	  function createChart(id, prefix, labelString) {
+  
+		// Assemble the datasets
+		var datasets = [];
+		chartDefinitions.modes.forEach(mode => {
+		  datasets.push({
+			label: mode[0],
+			data: createArray(prefix + '_' + mode[1], chartDefinitions.suffixes),
+			backgroundColor: mode[2],
+			borderColor: mode[3],
+			borderWidth: 1
+		  })
+		});
+  
+		return new Chart(document.getElementById(id).getContext('2d'), {
+		  type: 'bar',
+		  data: {
 			labels: chartDefinitions.labels,
 			datasets: datasets
-		},
-		options: {
+		  },
+		  options: {
 			scales: {
-				y: {
-				  stacked: true,
-				  title: {
-            display: true,
-            text: labelString
-          },
-				  scaleLabel: {
-            display: true
-          },
-					ticks: {
-						beginAtZero: true,
-						
-					}
+			  y: {
+				stacked: true,
+				title: {
+				  display: true,
+				  text: labelString
 				},
-				x: {
-				  stacked: true
+				scaleLabel: {
+				  display: true
 				},
+				ticks: {
+				  beginAtZero: true,
+				}
+			  },
+			  x: {
+				stacked: true
+			  },
 			},
 			responsive: true,
 			maintainAspectRatio: false
-		}
-	})
+		  }
+		})
+	  }
+  
+	  // Create each chart
+	  chartDefinitions.charts.forEach((chartDefinition, i) => {
+		charts[i] = createChart(chartDefinition[0], chartDefinition[1], chartDefinition[2]);
+	  });
+	};
   }
-
-	
-	// Create each chart
-	chartDefinitions.charts.forEach ((chartDefinition, i) => {
-		charts[i] = createChart (chartDefinition[0], chartDefinition[1], chartDefinition[2]);
-	});
-	
-};
-
-}
-
-schoolsModal ();
+  
+  schoolsModal();
