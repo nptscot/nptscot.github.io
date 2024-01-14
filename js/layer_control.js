nptscot/nptscot.github.io

commit f13b4058bf90f860eada183af7d9ee9b80b2ad76

Layers: Convert switch_data_zones to unobtrusive, bringing it into scope of main toggleLayer change handler

js/layer_control.js
@@ -492,14 +492,15 @@ function switch_style(){
     // Manage placenames
     placenames();
     
-    // Handle layer checkboxes and sliders
+    // Handle layer change controls, each marked with the updatelayer class
     document.querySelectorAll('.updatelayer').forEach((input) => {
       input.addEventListener('change', function(e) {
         layerId = e.target.id;
-        layerId = layerId.replace (/simplifiedcheckbox$/, '');    // Simplification checkbox, e.g. rnetsimplifiedcheckboxn => rnet
-        layerId = layerId.replace (/checkbox$/, '');      // Checkboxes, e.g. data_zonescheckbox => data_zones
-        layerId = layerId.replace (/_slider-.+$/, '');    // Slider hidden inputs, e.g. rnet_slider-quietness => rnet
-        layerId = layerId.replace (/_[^_]+_input$/, '');  // Dropdowns, e.g. rnet_purpose_input => rnet
+        layerId = layerId.replace (/simplifiedcheckbox$/, '');  // Simplification checkbox, e.g. rnetsimplifiedcheckboxn => rnet
+        layerId = layerId.replace (/checkbox$/, '');            // Checkboxes, e.g. data_zonescheckbox => data_zones
+        layerId = layerId.replace (/_slider-.+$/, '');          // Slider hidden inputs, e.g. rnet_slider-quietness => rnet
+        layerId = layerId.replace (/_selector$/, '');           // Dropdowns, e.g. data_zones_selector => data_zones   #!# Should be input, but currently data_zones_input would clash with rnet_*_input on next line
+        layerId = layerId.replace (/_[^_]+_input$/, '');        // Dropdowns, e.g. rnet_purpose_input => rnet
         toggleLayer(layerId);
       });
     });

index.html
@@ -225,7 +225,7 @@
   <p><label><input type="checkbox" id="data_zonescheckbox" class="updatelayer" value="false">Show Layer</label></p>
 
   <p>
-    <select id="data_zone_input" name="data_zone_input" onchange="switch_data_zones()" aria-label="Data zone layer">
+    <select id="data_zones_selector" name="data_zones_selector" class="updatelayer" aria-label="Data zone layer">
       <option value="pcycle">% commuter cycling (baseline)</option>
       <option value="pcycle_go_dutch" selected>% commuter cycling (Go Dutch)</option>
       <option value="population_density">Population density (per hectare)</option>

commit f53221a70062d074a9199ebb05c231e64780f97c

Layers: Fix field rename regression in f13b4058bf90f860eada183af7d9ee9b80b2ad76

js/layer_control.js
@@ -766,7 +766,7 @@ function switch_data_zones() {
   };
   
   var dataZonesCheckBox = document.getElementById('data_zonescheckbox');
-  var layerId = document.getElementById("data_zone_input").value;
+  var layerId = document.getElementById("data_zones_selector").value;
   var daysymetricmode = document.getElementById('data_zones_checkbox_dasymetric');
   
   // Update the Legend - Do this even if map layer is off