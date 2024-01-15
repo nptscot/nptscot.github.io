444fd16ffa3e066463b8f9ed8304cbef7bd1ac26
mvl22 <martin@lucas-smith.co.uk>
2024-01-15 18:09:11 +0000

    Modals: Migrate welcome to generic modal handler

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,6 @@
-// Get the <span> element that closes the welcome or help
-var span = document.getElementsByClassName("closewelcome")[0];
-var spanhelp = document.getElementsByClassName("closehelp")[0];
+// Create modal for help
+newModal ('welcome');
+
 
 function formatAsUKDate(date) {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -11,33 +11,19 @@ function formatAsUKDate(date) {
 document.getElementById("updatedate").innerHTML = 'Last Updated: ' + formatAsUKDate(document.lastModified) + '. You may need to <a href="https://www.minitool.com/news/f5-vs-ctrl-f5.html">clear your browser cache</a> to see the latest updates.';
 
 // When the user clicks on <span> (x), close the welcome and help
-span.onclick = function() {
-  document.getElementById("welcome").style.display = "none";
-  toggle_overlay(false);
-};
-
+var spanhelp = document.getElementsByClassName("closehelp")[0];
 spanhelp.onclick = function() {
   document.getElementById("help").style.display = "none";
   toggle_overlay(false);
 };
 
-// When the user clicks anywhere outside of the welcome, close it
-//window.onclick = function(event) {
-//  if(!welcome.contains(event.target)){
-//	welcome.style.display = "none";
-//	toggle_overlay(false);
-//  }
-//};
-
 function toggle_overlay(on) {
-  //console.log(on)
   if(on){
     document.getElementById("overlay").style.display = "block";
   } else {
     document.getElementById("overlay").style.display = "none";
   }
 }
-
 
 
 
