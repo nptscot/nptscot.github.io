a025bab550b5492df73fcd1abe8fdd4d6607456c
mvl22 <martin@lucas-smith.co.uk>
2024-01-21 21:55:49 +0000

    Accordion: Encapsulate layerControlsBoxUi

--- js/accordion.js
+++ js/accordion.js
@@ -1,25 +1,15 @@
 
 
-accordion ();
-
-function accordion () {
-  
-  document.querySelectorAll('.accordion').forEach (element => {
-    element.addEventListener('click', function() {
-      
-      // Toggle between adding and removing the "active" class, to highlight the button that controls the panel
-      this.classList.toggle('active');
-      
-      // Toggle between hiding and showing the active panel
-      var panel = this.nextElementSibling;
-      panel.style.display = (panel.style.display == 'block' ? 'none' : 'block');
-    });
-  });
-}
+layerControlsBoxUi ();
 
 
-// Manage layer controls box if present
-if (document.getElementById('rightbox')) {
+
+
+// Function to manage the layer controls box UI
+function layerControlsBoxUi ()
+{
+  // Enable the accordion functionality
+  accordion ();
   
   // Show the layer controls box, and open up the route network part of this
   showlayercontrols(true);
@@ -34,18 +24,34 @@ if (document.getElementById('rightbox')) {
   document.querySelector ('#rightbox button.close-button').addEventListener ('click', function () {
     showlayercontrols (false);
   });
+  
+  /* Show and hide UI */
+  function showlayercontrols(show){
+    
+    // Toggle box
+    var box = document.getElementById('rightbox');
+    box.style.display = (show ? 'block' : 'none');
+    
+    var boxbutton = document.getElementById('showrightbox');
+    boxbutton.style.display = (show ? 'none' : 'block');
+  }
 }
 
 
-/* Show and hide UI */
-function showlayercontrols(show){
+// Function to manage an accordion
+function accordion () {
   
-  // Toggle box
-  var box = document.getElementById('rightbox');
-  box.style.display = (show ? 'block' : 'none');
-  
-  var boxbutton = document.getElementById('showrightbox');
-  boxbutton.style.display = (show ? 'none' : 'block');
+  document.querySelectorAll('.accordion').forEach (element => {
+    element.addEventListener('click', function() {
+      
+      // Toggle between adding and removing the 'active' class, to highlight the button that controls the panel
+      this.classList.toggle('active');
+      
+      // Toggle between hiding and showing the active panel
+      var panel = this.nextElementSibling;
+      panel.style.display = (panel.style.display == 'block' ? 'none' : 'block');
+    });
+  });
 }
 
 
