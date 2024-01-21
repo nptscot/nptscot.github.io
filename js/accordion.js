90fa88b29f924f38c2501abc67b1f1c1dfffc70a
mvl22 <martin@lucas-smith.co.uk>
2024-01-21 21:50:06 +0000

    GUI: Branch GUI layout management functionality to new file

--- js/accordion.js
+++ js/accordion.js
@@ -56,13 +56,3 @@ function showbasemapcontrol(){
 }
 
 
-// Main menu responsive display
-document.getElementById ('expandtopnav').addEventListener ('click', function (e) {
-  var x = document.getElementById('myTopnav');
-  if (x.className == 'topnav') {
-    x.classList.add ('responsive');
-  } else {
-    x.classList.remove ('responsive');
-  }
-  e.preventDefault ();
-});
