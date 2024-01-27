ed670bf10a5d93275a2e51cce2212ee27b4e9956
mvl22 <martin@lucas-smith.co.uk>
2024-01-27 21:36:01 +0000

    Popup: Simplify value pre-processing

--- js/popup.js
+++ js/popup.js
@@ -1,41 +1,39 @@
 
-// Convert small to <9
-function suppresssmall(x) {
-  return (x < 10 ? '≤9' : x);
-}
-
 // Click on rnet for popup
+// #!# Gradient and Quietness are capitalised
 map.on('click', 'rnet', function (e) {
   
-  var coordinates = e.lngLat;
-
-  // Process all the properties
-  // Processed gradient and quietness but ignored
-  const properties = e.features[0].properties;
-  const prop = {};
-
-  for (const property in properties) {
-    prop[property] = suppresssmall(properties[property]);
-  }
-
-  var Gradient = e.features[0].properties.Gradient;
-  var Quietness = e.features[0].properties.Quietness;
-
-  var layerPurpose = document.getElementById("rnet_purpose_input").value;
-  var layerScenario = document.getElementById("rnet_scenario_input").value;
-  var layerType = document.getElementById("rnet_type_input").value;
-
-  var layername = layerPurpose + "_" + layerType + "_" + layerScenario;
-
-  var ncycle = suppresssmall(e.features[0].properties[layername]);
+  const coordinates = e.lngLat;
+  
+  const feature = e.features[0];
+  
+  // Format key fields as percentages
+  const percentageFields = ['Gradient', 'Quietness'];
+  percentageFields.forEach (field => {
+    feature.properties[field] += '%';
+  });
+    
+  // Suppress small numeric values
+  Object.entries (feature.properties).forEach (([key, value]) => {
+    if (Number.isFinite(value) && (value < 10)) {   // Number check means strings/percentages/etc. get skipped
+      feature.properties[key] = '≤9';
+    }
+  });
+  
+  // Determine the number of cyclists for this layer
+  var layerPurpose = document.getElementById('rnet_purpose_input').value;
+  var layerType = document.getElementById('rnet_type_input').value;
+  var layerScenario = document.getElementById('rnet_scenario_input').value;
+  var layername = layerPurpose + '_' + layerType + '_' + layerScenario;
+  var ncycle = feature.properties[layername];
   
   var streetViewUrl = 'https://maps.google.com/maps?q=&layer=c&cbll=' +  coordinates.lat + ',' + coordinates.lng + '&cbp=11,0,0,0,0';
   var osmUrl = 'https://www.openstreetmap.org/#map=19/' + coordinates.lat + '/' + coordinates.lng;
   
   var description = '<div class="mappopup">' + 
   '<p>Cyclists: ' + ncycle + '</p>' +
-  '<p>Gradient: ' + Gradient + '%</p>' +
-  '<p>Cycle-friendliness: ' + Quietness + '%</p>' +
+  '<p>Gradient: ' + feature.properties.Gradient + '</p>' +
+  '<p>Cycle-friendliness: ' + feature.properties.Quietness + '</p>' +
   '<p><a class="externallink" target="_blank" href="' + streetViewUrl + '">Google Street View <i class="fa fa-external-link" aria-hidden="true"></i></a> ' +
   '<a class="externallink" target="_blank" href="' + osmUrl + '">OpenStreetMap <i class="fa fa-external-link" aria-hidden="true"></i></a></p>' +
 
@@ -44,20 +42,20 @@ map.on('click', 'rnet', function (e) {
 
   '<h4>Fast/Direct network</h4>' +
   '<table><tr><th></th><th>Baseline</th><th>Go Dutch</th><th>Ebikes</th></tr>' +
-  '<tr><th>All</td><td>' + prop.all_fastest_bicycle + '</td><td>' + prop.all_fastest_bicycle_go_dutch + '</td><td>' + prop.all_fastest_bicycle_ebike + '</td></tr>' + 
-  '<tr><th>Commute</td><td>' + prop.commute_fastest_bicycle + '</td><td>' + prop.commute_fastest_bicycle_go_dutch + '</td><td>' + prop.commute_fastest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Primary</td><td>' + prop.primary_fastest_bicycle + '</td><td>' + prop.primary_fastest_bicycle_go_dutch + '</td><td>' + prop.primary_fastest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Secondary</td><td>' + prop.secondary_fastest_bicycle + '</td><td>' + prop.secondary_fastest_bicycle_go_dutch + '</td><td>' + prop.secondary_fastest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Utility</td><td>' + prop.utility_fastest_bicycle + '</td><td>' + prop.utility_fastest_bicycle_go_dutch + '</td><td>' + prop.utility_fastest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>All</td><td>' + feature.properties.all_fastest_bicycle + '</td><td>' + feature.properties.all_fastest_bicycle_go_dutch + '</td><td>' + feature.properties.all_fastest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Commute</td><td>' + feature.properties.commute_fastest_bicycle + '</td><td>' + feature.properties.commute_fastest_bicycle_go_dutch + '</td><td>' + feature.properties.commute_fastest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Primary</td><td>' + feature.properties.primary_fastest_bicycle + '</td><td>' + feature.properties.primary_fastest_bicycle_go_dutch + '</td><td>' + feature.properties.primary_fastest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Secondary</td><td>' + feature.properties.secondary_fastest_bicycle + '</td><td>' + feature.properties.secondary_fastest_bicycle_go_dutch + '</td><td>' + feature.properties.secondary_fastest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Utility</td><td>' + feature.properties.utility_fastest_bicycle + '</td><td>' + feature.properties.utility_fastest_bicycle_go_dutch + '</td><td>' + feature.properties.utility_fastest_bicycle_ebike + '</td></tr>' +
   '</table>' +
 
   '<h4>Quiet/Indirect network</h4>' + 
   '<table><tr><th></th><th>Baseline</th><th>Go Dutch</th><th>Ebikes</th></tr>' +
-  '<tr><th>All</td><td>' + prop.all_quietest_bicycle + '</td><td>' + prop.all_quietest_bicycle_go_dutch + '</td><td>' + prop.all_quietest_bicycle_ebike + '</td></tr>' + 
-  '<tr><th>Commute</td><td>' + prop.commute_quietest_bicycle + '</td><td>' + prop.commute_quietest_bicycle_go_dutch + '</td><td>' + prop.commute_quietest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Primary</td><td>' + prop.primary_quietest_bicycle + '</td><td>' + prop.primary_quietest_bicycle_go_dutch + '</td><td>' + prop.primary_quietest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Secondary</td><td>' + prop.secondary_quietest_bicycle + '</td><td>' + prop.secondary_quietest_bicycle_go_dutch + '</td><td>' + prop.secondary_quietest_bicycle_ebike + '</td></tr>' +
-  '<tr><th>Utility</td><td>' + prop.utility_quietest_bicycle + '</td><td>' + prop.utility_quietest_bicycle_go_dutch + '</td><td>' + prop.utility_quietest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>All</td><td>' + feature.properties.all_quietest_bicycle + '</td><td>' + feature.properties.all_quietest_bicycle_go_dutch + '</td><td>' + feature.properties.all_quietest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Commute</td><td>' + feature.properties.commute_quietest_bicycle + '</td><td>' + feature.properties.commute_quietest_bicycle_go_dutch + '</td><td>' + feature.properties.commute_quietest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Primary</td><td>' + feature.properties.primary_quietest_bicycle + '</td><td>' + feature.properties.primary_quietest_bicycle_go_dutch + '</td><td>' + feature.properties.primary_quietest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Secondary</td><td>' + feature.properties.secondary_quietest_bicycle + '</td><td>' + feature.properties.secondary_quietest_bicycle_go_dutch + '</td><td>' + feature.properties.secondary_quietest_bicycle_ebike + '</td></tr>' +
+  '<tr><th>Utility</td><td>' + feature.properties.utility_quietest_bicycle + '</td><td>' + feature.properties.utility_quietest_bicycle_go_dutch + '</td><td>' + feature.properties.utility_quietest_bicycle_ebike + '</td></tr>' +
   '</table>' +
     
   '</div>';
