nptscot/nptscot.github.io

commit d2a492154e8feefe1334d2bf24e588fe7817ad3c

Map: Move basemap control into map function

js/map.js
@@ -63,7 +63,10 @@ function createMap ()
       div.className = 'maplibregl-ctrl maplibregl-ctrl-group';
       div.innerHTML = '<button aria-label="Change basemap"><img src="/images/basemap.svg" title="Change basemap" /></button>';
       div.addEventListener('contextmenu', (e) => e.preventDefault());
-      div.addEventListener('click', () => showbasemapcontrol());
+      div.addEventListener('click', function () {
+        var box = document.getElementById('basemapcontrol');
+        box.style.display = (box.style.display == 'none' ? 'block' : 'none');
+      });
       return div;
     }
   }

js/accordion.js
@@ -53,12 +53,3 @@ function accordion () {
     });
   });
 }
-
-
-// Basemap control
-function showbasemapcontrol(){
-    var box = document.getElementById('basemapcontrol');
-    box.style.display = (box.style.display == 'none' ? 'block' : 'none');
-}
-
-

commit ebc4e47dd4291570fe1f9fe5af732c53d47b278a

Basemap: Fix apparent regression in d2a492154e8feefe1334d2bf24e588fe7817ad3c or fe742c9b110fb72214545a9e7d83ce3af406893f

js/map.js
@@ -65,7 +65,7 @@ function createMap ()
       div.addEventListener('contextmenu', (e) => e.preventDefault());
       div.addEventListener('click', function () {
         var box = document.getElementById('basemapcontrol');
-        box.style.display = (box.style.display == 'none' ? 'block' : 'none');
+        box.style.display = (window.getComputedStyle(box).display == 'none' ? 'block' : 'none');
       });
       return div;
     }