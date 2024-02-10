9fca701f18f7740dcfd7a85fd6b54149b041a9dc
mvl22 <martin@lucas-smith.co.uk>
2024-02-10 19:50:10 +0000

    Date: Surround last updated date in a function and avoid layout moving by making a placeholder in the HTML

--- js/welcome.js
+++ js/welcome.js
@@ -8,5 +8,11 @@ function formatAsUKDate(date) {
 }
 
 
-document.getElementById("updatedate").innerHTML = 'Last Updated: ' + formatAsUKDate(document.lastModified) + '. You may need to <a href="https://www.minitool.com/news/f5-vs-ctrl-f5.html">clear your browser cache</a> to see the latest updates.';
+updatedate ();
 
+function updatedate ()
+{
+  let text = 'Last updated: ' + formatAsUKDate(document.lastModified) + '.';
+  text += ' You may need to <a href="https://www.minitool.com/news/f5-vs-ctrl-f5.html">clear your browser cache</a> to see the latest updates.'
+  document.getElementById('updatedate').innerHTML = text;
+}
