24c7c77d89097c528de1ee6ba2c72c4fda3fd109
mvl22 <martin@lucas-smith.co.uk>
2024-02-11 23:16:28 +0000

    JS: Reorder file (no logic changes)

--- src/nptui.js
+++ src/nptui.js
@@ -83,56 +83,6 @@ const nptUi = (function () {
 		},
 		
 		
-		// Function to manage modal dialogs
-		newModal: function (modalId)
-		{
-			// Identify the modal
-			const modal = document.getElementById(modalId);
-			
-			// When the user clicks on <span> (x), close the modal
-			const closeButton = document.querySelector('#' + modalId + ' .modal-close');
-			closeButton.addEventListener('click', function () {
-				hide();
-			});
-			
-			// Treat clicking outside of the modal as implied close
-			window.addEventListener('click', function (event) {
-				if (event.target == modal || event.target.id == 'overlay') {
-					hide();
-				}
-			});
-			
-			// Treat escape key as implied close
-			window.addEventListener('keyup', function (event) {
-				if (event.key == 'Escape') {
-					if (window.getComputedStyle(modal).display == 'block') { // I.e. is displayed
-						hide();
-					}
-				}
-			});
-			
-			// Show
-			const show = function ()
-			{
-				document.getElementById('overlay').style.display = 'block';
-				modal.style.display = 'block';
-			};
-			
-			// Hide
-			const hide = function ()
-			{
-				modal.style.display = 'none';
-				document.getElementById('overlay').style.display = 'none';
-			};
-			
-			// Accessor functions
-			return {
-				show: show,
-				hide: hide
-			};
-		},
-		
-		
 		// Function to manage an accordion
 		accordion: function ()
 		{
@@ -1039,6 +989,56 @@ const nptUi = (function () {
 				//console.log ("Cookie status: '" + cookie + "'");
 				cookiewarning.style.display = (cookie === '' ? 'block' : 'none');
 			}
+		},
+		
+		
+		// Function to manage modal dialogs
+		newModal: function (modalId)
+		{
+			// Identify the modal
+			const modal = document.getElementById(modalId);
+			
+			// When the user clicks on <span> (x), close the modal
+			const closeButton = document.querySelector('#' + modalId + ' .modal-close');
+			closeButton.addEventListener('click', function () {
+				hide();
+			});
+			
+			// Treat clicking outside of the modal as implied close
+			window.addEventListener('click', function (event) {
+				if (event.target == modal || event.target.id == 'overlay') {
+					hide();
+				}
+			});
+			
+			// Treat escape key as implied close
+			window.addEventListener('keyup', function (event) {
+				if (event.key == 'Escape') {
+					if (window.getComputedStyle(modal).display == 'block') { // I.e. is displayed
+						hide();
+					}
+				}
+			});
+			
+			// Show
+			const show = function ()
+			{
+				document.getElementById('overlay').style.display = 'block';
+				modal.style.display = 'block';
+			};
+			
+			// Hide
+			const hide = function ()
+			{
+				modal.style.display = 'none';
+				document.getElementById('overlay').style.display = 'none';
+			};
+			
+			// Accessor functions
+			return {
+				show: show,
+				hide: hide
+			};
 		},
 		
 		
