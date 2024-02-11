3856b52c50810ab4d30b2b08e0a8d0b2165de7ad
mvl22 <martin@lucas-smith.co.uk>
2024-02-11 15:59:22 +0000

    nptUi: Move analytics code into class

--- src/nptui.js
+++ src/nptui.js
@@ -21,12 +21,95 @@ var nptUi = (function () {
 			// Populate the settings class property
 			_settings = settings;
 			
-			//...
+			// Manage analytics cookie setting
+			nptUi.manageAnalyticsCookie ();
+			
 		},
 		
 		
-		// Functions start here, comma-separated
+		// function to manage analytics cookie setting
+		manageAnalyticsCookie: function ()
+		{
+			// Disable tracking if the opt-out cookie exists.
+			var disableStr = 'ga-disable-' + _settings.gaProperty;
+			if (document.cookie.indexOf(disableStr + '=true') > -1) {
+				window[disableStr] = true;
+			}
+
+			// Define the cookie name
+			const cookieName = 'NPTtrack';
+
+			// Handle cookie warning buttons
+			document.querySelectorAll('#cookiewarning button').forEach(function (button) {
+				button.addEventListener('click', function (e) {
+					cookieButton(button.value);
+				});
+			});
+
+			// Show the cookie warning
+			showCookieWarning();
+
+
+			// Opt-out function
+			function gaOptout ()
+			{
+				document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/; SameSite=None; Secure';
+				window[disableStr] = true;
+			}
+
+
+			// Warning Control
+			function cookieButton (accepted)
+			{
+				if (accepted) {
+					nptUi.setCookie(cookieName, 'true');
+				} else {
+					//alert("Tracking Op-Out Disabled");
+					gaOptout();
+					nptUi.setCookie(cookieName, 'false');
+				}
+
+				var cookiewarning = document.getElementById("cookiewarning");
+				cookiewarning.style.display = "none";
+			}
+
+
+			// Cookie warning
+			function showCookieWarning ()
+			{
+				var cookiewarning = document.getElementById("cookiewarning");
+				var NPTcookie = nptUi.getCookie(cookieName);
+				console.log("Cookie status: '" + NPTcookie + "'");
+				cookiewarning.style.display = (NPTcookie === '' ? 'block' : 'none');
+			}
+		},
 		
+		
+		// Generic cookie managment functions
+		setCookie: function (name, value)
+		{
+			var d = new Date();
+			d.setTime(d.getTime() + (1000 * 24 * 60 * 60 * 1000));
+			var expires = 'expires=' + d.toUTCString();
+			document.cookie = name + '=' + value + ';' + expires + ';path=/';
+		},
+		
+		
+		getCookie: function (name)
+		{
+			var name = name + '=';
+			var ca = document.cookie.split(';');
+			for (var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') {
+					c = c.substring(1);
+				}
+				if (c.indexOf(name) === 0) {
+					return c.substring(name.length, c.length);
+				}
+			}
+			return "";
+		}
 	};
 	
 } ());
