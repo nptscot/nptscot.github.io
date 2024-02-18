6228532cd331f6038f40ced69414d826107e3bf0
mvl22 <martin@lucas-smith.co.uk>
2024-02-18 18:58:53 +0000

    Input namings: Specify explicit data-layer for _input controls

--- src/nptui.js
+++ src/nptui.js
@@ -473,8 +473,7 @@ const nptUi = (function () {
 						// #!# The input IDs should be standardised, to replace this list of regexp matches
 						layerId = layerId.replace(/_checkbox_.+$/, ''); // Checkboxes, e.g. data_zones_checkbox_dasymetric => data_zones
 						layerId = layerId.replace(/_slider-.+$/, ''); // Slider hidden inputs, e.g. rnet_slider-quietness => rnet
-						layerId = layerId.replace(/_selector$/, ''); // Dropdowns, e.g. data_zones_selector => data_zones   #!# Should be input, but currently data_zones_input would clash with rnet_*_input on next line
-						layerId = layerId.replace(/_[^_]+_input$/, ''); // Dropdowns, e.g. rnet_purpose_input => rnet
+						layerId = layerId.replace(/_selector$/, ''); // Dropdowns, e.g. data_zones_selector => data_zones   #!# Should be input
 						
 						// Use explicit layer ID if specified
 						if (input.dataset.layer) {
